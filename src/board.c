7f1d6a96f661a39a704abd93601da1806fb96c44
irjkf129 <sedava444@gmail.com>
2020-03-15 03:03:01 +0700

    Add Functions for input and parsing input data, add struct Parsing

--- src/board.c
+++ src/board.c
@@ -1,4 +1,7 @@
 #include "board.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void initiate_board(char board[][8])
 {
@@ -22,4 +25,87 @@ void initiate_board(char board[][8])
     board[0][5] = 'B';
     board[0][6] = 'N';
     board[0][7] = 'R';
+}
+
+void parsing_white(Parsing* turn, int* start, int len)
+{
+    int count = 0;
+    if (turn->data[*start] - 'a' >= 0) { //все фигуры с большой буквы, -> если
+                                         //первая буква - маленькая - это пешка
+        turn->white_figure = 'p';
+        turn->white_turn[0] = turn->data[*start] - 'a';
+        count++;
+    } else {
+        turn->white_figure = turn->data[*start];
+    }
+
+    for (int i = *start + 1; i < len; i++) {
+        if (turn->data[i] == ' ') { // eсли смена хода, выходим из цикла
+            *start = i;
+            break;
+        }
+        if (turn->data[i] == 'x'
+            || turn->data[i] == '-') { //если тип хода, то записываем его и на
+                                       //следующую итерацию
+            turn->type_turn_white = turn->data[i];
+            continue;
+        }
+
+        if (isalpha(turn->data[i])) {
+            turn->white_turn[count] = turn->data[i] - 'a';
+            count++;
+        }
+
+        if (isdigit(turn->data[i])) {
+            turn->white_turn[count] = turn->data[i] - '0';
+            count++;
+        }
+    }
+}
+
+void parsing_black(Parsing* turn, int* start, int len)
+{
+    int count = 0;
+    if (turn->data[*start] - 'a' >= 0) { //все фигуры с большой буквы, -> если
+                                         //первая буква - маленькая - это пешка
+        turn->black_figure = 'p';
+        turn->black_turn[0] = turn->data[*start] - 'a';
+        count++;
+
+    } else {
+        turn->black_figure = turn->data[*start];
+    }
+
+    for (int i = *start + 1; i <= len; i++) {
+        if (turn->data[i] == 'x' || turn->data[i] == '-') {
+            turn->type_turn_black = turn->data[i];
+            continue;
+        }
+
+        if (isalpha(turn->data[i])) {
+            turn->black_turn[count] = turn->data[i] - 'a';
+            count++;
+        }
+
+        if (isdigit(turn->data[i])) {
+            turn->black_turn[count] = turn->data[i] - '0';
+            count++;
+        }
+    }
+}
+
+void input_data(Parsing* turn)
+{
+    char i = getchar();
+    int len = 1;
+    turn->data[0] = i;
+    while (i != '\n') {
+        i = getchar();
+        turn->data[len] = i;
+        len++;
+    }
+    int count = 0;
+    parsing_white(turn, &count, len);
+    count++;
+    parsing_black(turn, &count, len);
 }
--- src/main.c
+++ src/main.c
@@ -1,10 +1,28 @@
 #include "board.h"
 #include "board_print_plain.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
     char board[8][8] = {{' '}};
+    Parsing* turn = malloc(sizeof(Parsing));
     initiate_board(board);
     print_board(board);
+    input_data(turn);
+    printf("White:\n");
+    for (int i = 0; i < 4; i++) {
+        printf("%d ", turn->white_turn[i]);
+    }
+    printf("\nBlack:\n");
+    for (int i = 0; i < 4; i++) {
+        printf("%d ", turn->black_turn[i]);
+    }
+    printf("\nblack = %c, white = %c\n",
+           turn->black_figure,
+           turn->white_figure);
+    printf("Black_type = %c\nWhite type = %c\n",
+           turn->type_turn_black,
+           turn->type_turn_white);
     return 0;
 }
