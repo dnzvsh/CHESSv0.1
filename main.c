7197a73b03ecd7226bd6809793467c96d87b6dfb
dnzvsh <zuevdanil@gmail.com>
2020-03-14 14:16:10 +0700

    Add lab1

---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void initiate_board(char board[][8])
+{
+    board[7][1] = 'n';
+    board[7][2] = 'b';
+    board[7][3] = 'k';
+    board[7][4] = 'q';
+    board[7][5] = 'b';
+    board[7][6] = 'n';
+    board[7][7] = 'r';
+    board[7][0] = 'r';
+    for (int i = 0; i < 8; i++) {
+        board[6][i] = 'p';
+        board[1][i] = 'P';
+    }
+    board[0][0] = 'R';
+    board[0][1] = 'N';
+    board[0][2] = 'B';
+    board[0][3] = 'Q';
+    board[0][4] = 'K';
+    board[0][5] = 'B';
+    board[0][6] = 'N';
+    board[0][7] = 'R';
+}
+
+void print_board(char board[][8])
+{
+    printf("   A B C D E F G H\n   _______________\n");
+    for (int i = 0; i < 8; i++) {
+        printf("%d |", i + 1);
+        for (int j = 0; j < 8; j++) {
+            printf("%c ", board[i][j]);
+        }
+        printf("\n");
+    }
+    printf("   _______________\n   A B C D E F G H\n");
+}
+
+int main()
+{
+    char board[8][8] = {{' '}};
+    initiate_board(board);
+    print_board(board);
+    return 0;
+}
