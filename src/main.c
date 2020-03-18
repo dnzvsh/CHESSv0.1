ee11afc7217771af0679d804a58a03c652facfaf
Malfak <puteev02@mail.ru>
2020-03-18 23:40:39 +0700

    Now the program does not close after 1 round

--- src/main.c
+++ src/main.c
@@ -11,34 +11,36 @@ int main()
     turn->round = 0;
     initiate_board(board);
     print_board(board);
-    input_data(turn);
-    printf("White:\n");
-    for (int i = 0; i < 4; i++) {
-        printf("%d ", turn->white_turn[i]);
-    }
-    printf("\nBlack:\n");
-    for (int i = 0; i < 4; i++) {
-        printf("%d ", turn->black_turn[i]);
-    }
-    printf("\nblack = %c, white = %c\n",
-           turn->black_figure,
-           turn->white_figure);
-    printf("Black_type = %c\nWhite type = %c\n",
-           turn->type_turn_black,
-           turn->type_turn_white);
-    t = check(turn, board);
-    if (t) {
-        return 0;
-    }
-    t = white_pawn(turn, board);
-    print_board(board);
-    if (t) {
-        return 0;
-    }
-    t = black_pawn(turn, board);
-    print_board(board);
-    if (t) {
-        return 0;
+    for (int j = 0; j < 5950; j++) {
+        input_data(turn);
+        printf("White:\n");
+        for (int i = 0; i < 4; i++) {
+            printf("%d ", turn->white_turn[i]);
+        }
+        printf("\nBlack:\n");
+        for (int i = 0; i < 4; i++) {
+            printf("%d ", turn->black_turn[i]);
+        }
+        printf("\nblack = %c, white = %c\n",
+               turn->black_figure,
+               turn->white_figure);
+        printf("Black_type = %c\nWhite type = %c\n",
+               turn->type_turn_black,
+               turn->type_turn_white);
+        t = check(turn, board);
+        if (t) {
+            return 0;
+        }
+        t = white_pawn(turn, board);
+        print_board(board);
+        if (t) {
+            return 0;
+        }
+        t = black_pawn(turn, board);
+        print_board(board);
+        if (t) {
+            return 0;
+        }
     }
     return 0;
 }
